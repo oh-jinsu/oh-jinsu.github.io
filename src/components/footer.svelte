178b33c13265f7721ae4622c46bc4749e59042b6
오진수 <ohjinsu@codersproduct.com>
2022-11-10 16:30:11 +0900

    Remove the top button temporarily

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,4 @@
 <footer>
-    <p class="top-button">
-        <a href="#top">맨위로</a>
-    </p>
     <hr />
     <p>
         © { new Date().getFullYear()} 오진수.
@@ -13,9 +10,5 @@
         max-width: var(--scren-width);
 
         margin: 32px auto;
-    }
-
-    .top-button {
-        text-align: center;
     }
 </style>
