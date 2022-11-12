e0c0ec7ff1ee274725b28c3744bf30419f376c14
오진수 <ohjinsu@codersproduct.com>
2022-11-12 14:23:47 +0900

    Add a post

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -1,22 +1,24 @@
 <section>
     <p>궁금한 점이 있으신가요? 언제든지 아래 이메일로 연락하세요.</p>
     <p>
-        이메일: 
-        <a href="mailto:ohjinsu98@icloud.com">ohjinsu98@icloud.com</a>
+        <span>
+            이메일: 
+            <a href="mailto:ohjinsu98@icloud.com">ohjinsu98@icloud.com</a>
+        </span>
+        <br />
+        <span>
+            깃허브: 
+            <a href="https://github.com/oh-jinsu" target="_blank" rel="noreferrer">https://github.com/oh-jinsu</a>
+        </span>
     </p>
-    <!-- <br /> -->
-    <!-- <span>
-        깃허브: 
-        <a href="https://github.com/oh-jinsu" target="_blank" rel="noreferrer">https://github.com/oh-jinsu</a>
-    </span> -->
 </section>
 
 <style>
     section {
-        border: 4px ridge;
+        border: 2px dotted gray;
 
         padding: 0 16px;
 
-        margin: 32px 0;
+        margin: 32px 8px;
     }
 </style>
